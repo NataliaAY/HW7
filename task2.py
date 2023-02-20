cce8b5f019ba935d46b9c4c884eea2724560df5e
Natalia <averegina@male.ru>
2023-02-20 22:02:01 +0300

    решена задача 2_кривые столбцы

--- task2.py
+++ task2.py
@@ -17,7 +17,7 @@ print_operation_table(lambda x, y: x * y)
 '''
 
 
-def print_operation_table(operation, num_rows=9, num_columns=9):
+def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         answer = []
         for j in range(1, num_columns + 1):
